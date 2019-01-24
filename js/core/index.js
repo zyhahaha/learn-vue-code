1a339c365b6e458671bd4cedea87d3e75b80cf15
zhaoyang <18355403288@163.com>
2019-01-24 19:06:00 +0800

    update

--- js/core/index.js
+++ js/core/index.js
@@ -16,28 +16,26 @@ function Vue(option) {
 }
 
 /////////////////////////////////// proxyData --  数据代理 ///////////////////////////////////////////
-function proxyData(key, getter){
+function proxyData(key, getter) {
   let self = this;
   Object.defineProperty(self, key, {
     configurable: false,
     enumerable: true,
-    get: function proxyGetter(){
+    get: function proxyGetter() {
       return self._data[key];
     },
-    set: function proxySetter(newVal){
+    set: function proxySetter(newVal) {
       self._data[key] = newVal;
     }
   });
 }
 
 /////////////////////////////////// observer --  监听属性 观察者模式 //////////////////////////////////
-function observer(){
-
-}
+function observer() {}
 
 /////////////////////////////////// compile -- 渲染页面 //////////////////////////////////////////////
 
-function compile(el){
+function compile(el) {
   let $el = document.querySelector(el);
 
   // compile
@@ -56,36 +54,105 @@ function compile(el){
 }
 
 // 编译元素
-function compileElement(el){
+function compileElement(el) {
   // childNodes
   let childNodes = el.childNodes;
+  // console.log(childNodes[0].attributes.forEach);
   childNodes.forEach(node => {
-    let text = node.textContent;
-    let reg = /\{\{(.*)\}\}/;
-    if (isTextNode(node) && reg.test(text)) {
-      let exp = RegExp.$1.trim();
-      let value = _getVMVal(this, exp); // 获取属性值
-      // console.log(value);
-      node.textContent = value;
+    if (isElementNode(node)) {
+      compileElementNode.call(this, node);
+    } else if (isTextNode(node)) {
+      compileText.call(this, node);
     }
 
+    // 如果有子级再次回调
     if (node.childNodes && node.childNodes.length) {
       compileElement.call(this, node);
     }
   });
 }
 
+// 编译文本
+function compileText(node) {
+  let text = node.textContent;
+  let reg = /\{\{(.*)\}\}/;
+  if (reg.test(text)) {
+    let exp = RegExp.$1.trim();
+    let value = _getVMVal(this, exp); // 获取属性值
+    // console.log(value);
+    node.textContent = value;
+  }
+}
+
+// 编译节点
+function compileElementNode(node) {
+  let nodeAttrs = node.attributes;
+  // console.log(nodeAttrs.forEach);
+  [].slice.call(nodeAttrs).forEach(attr => {
+    var attrName = attr.name;
+    if (isDirective(attrName)) {
+      var exp = attr.value;
+      var dir = attrName.substring(2);
+      // 事件指令
+      if (isEventDirective(dir)) {
+        compileUtil.eventHandler(node, this, exp, dir);
+      } else {
+        // 其他指令
+        compileUtil[dir] && compileUtil[dir](node, this, exp);
+      }
+      // 移除事件
+      node.removeAttribute(attrName);
+    }
+  });
+}
+
+// 指令集合
+let compileUtil = {
+  model: function(node, vm, exp) {
+    let self = this;
+    let val = _getVMVal(vm, exp);
+    node.addEventListener("input", e => {
+      var newVal = e.target.value;
+      if (val === newVal) {
+        return;
+      }
+      self._setVMVal(vm, exp, newValue);
+      val = newVal;
+    });
+  },
+  eventHandler: function(node, vm, exp, dir) {
+    var eventType = dir.split(":")[1];
+    console.log(eventType);
+  }
+};
+
 // get vm value
 function _getVMVal(vm, exp) {
   let val = vm;
-  exp = exp.split('.');
+  exp = exp.split(".");
   exp.forEach(k => {
     val = val[k];
   });
   return val;
 }
+function _setVMVal(vm, exp, value) {
+  var val = vm;
+  exp = exp.split(".");
+  exp.forEach(function(k, i) {
+    
+  });
+}
 
-// 判断node类型
+// 判断node类型 指令
+function isElementNode(node) {
+  return node.nodeType == 1;
+}
 function isTextNode(node) {
   return node.nodeType == 3;
+}
+function isDirective(attr) {
+  return attr.indexOf("v-") == 0;
+}
+function isEventDirective(dir) {
+  return dir.indexOf("on") === 0;
 }
